11a71d316c1ef9a0617ac891ba1ede10fa109d6c
George1879 <81874028+George1879@users.noreply.github.com>
2021-04-07 21:29:10 +0100

    Update agecalculator.py

--- agecalculator.py
+++ agecalculator.py
@@ -2,5 +2,5 @@ import datetime
 birth_year = int(input("What year were you born? "))
 current_year = datetime.datetime.now().year
 age = current_year - birth_year
-print("You are {age} years old now.")
+print(f"You are {age} years old now.")
 input()
