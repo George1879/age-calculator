3e5449685a2cda853a0a8e454a2fef3a0e2ce6c9
George1879 <81874028+George1879@users.noreply.github.com>
2021-04-08 11:04:54 +0100

    Update agecalculator.py

--- agecalculator.py
+++ agecalculator.py
@@ -1,6 +1,14 @@
 import datetime
-birth_year = int(input("What year were you born? "))
-current_year = datetime.datetime.now().year
-age = current_year - birth_year
-print(f"You are {age} years old now.")
-input()
+while True:
+    birth_year = int(input("What year were you born? "))
+    current_year = datetime.datetime.now().year
+    age = current_year - birth_year
+    print(f"You are {age} years old now.")
+    choice = input("Do you want to go again? (yes or no) ")
+    if choice == 'yes':
+        print("enjoy")
+    elif choice == 'no':
+        print ("Ok, quitting now")
+        break
+    else:
+        print("i'll assume that means yes")
